ea88c447094535e0566321f52b0b3bdeadec47aa
anglee <ang.lee@gmail.com>
2014-12-06 11:26:53 -0500

    Use underscore.string

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
+"use strict";
 var _ = require("lodash");
+var _s = require("underscore.string");
+_.mixin(_s.exports());
 var Q = require("q");
 
 Q.fcall(function() {
   console.log(_.range(1,10,2));
 });
 
-console.log("Hello, World!");
+console.log(_.trim("    Hello, World!"));
+
